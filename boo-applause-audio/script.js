dbb0b3b51f9ef32f6c7846ecf49b740c2b64987a
codinginrainbows <82886646+codinginrainbows@users.noreply.github.com>
2022-01-26 17:53:15 -0300

    Add files via upload

---
+++ boo-applause-audio/script.js
@@ -0,0 +1,14 @@
+const sounds = ['applause', 'boo'];
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+}) 
